e23148768a83e3aa814c9d5507321a844ec273d1
407-20-Saruta <54456@yupparaj.ac.th>
2026-07-31 11:54:56 +0700

    Update bmiApp.py

--- bmiApp.py
+++ bmiApp.py
@@ -6,13 +6,13 @@ st.write("กรอกข้อมูลน้ำหนักเเละส่�
 weight = st.number_input("กรอกน้ำหนักของคุณ (กิโลกรัม):")
 height_cm = st.number_input("กรอกส่วนสูงของคุณ (เซนติเมตร):")
 
-if st.button("คำนวนค่า BMI  "):
-   # เเปลงส่วนสูงจาก cm เป็น เมตร เเล้วคำนวน BMI
-   height_m = height_cm / 100
-   bmi = weight / (height_m ** 2)
+  if st.button("คำนวนค่า BMI  "):
+     # เเปลงส่วนสูงจาก cm เป็น เมตร เเล้วคำนวน BMI
+     height_m = height_cm / 100
+     bmi = weight / (height_m ** 2)
 
-   st.write("---")
-   st.header(f"ค่า BMI ของคุณคือ: **{bmi:.2f}**")
+     st.write("---")
+     st.header(f"ค่า BMI ของคุณคือ: **{bmi:.2f}**")
 
   if bmi < 18.5:
      st.warning(" คุณมีน้ำหนักน้อยกว่าเกณฑ์ (ผอม)")
